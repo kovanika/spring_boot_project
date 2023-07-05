5cc97b7618be9210bd3282f3fce6129ac4f4c747
p <p>
2023-07-05 11:42:35 +0300

    Add logger

--- src/main/java/com/example/spring_boot_project/UploadController.java
+++ src/main/java/com/example/spring_boot_project/UploadController.java
@@ -1,5 +1,7 @@
 package com.example.spring_boot_project;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -8,16 +10,17 @@ import java.io.*;
 
 @RestController
 public class UploadController {
+    private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
     @RequestMapping(value = "/files", method = RequestMethod.POST, consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public String saveFile(@RequestParam String name, @RequestParam String email,
-                                           @RequestParam MultipartFile document) throws IOException {
+                                           @RequestParam MultipartFile document) {
 
         File file = new File("C:\\Users\\user\\Desktop\\" + document.getOriginalFilename());
         try(OutputStream os = new FileOutputStream(file)){
             os.write(document.getBytes());
         }
-        catch (Exception e){
-            return "Exception!";
+        catch (IOException e){
+            logger.error(e.getMessage(), e);
         }
         return "file.getFile()";
     }
