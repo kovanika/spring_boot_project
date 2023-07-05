5fb0bfe2b2de8fd10a6b7d26e7cbec2a92fabe01
p <p>
2023-07-05 13:49:01 +0300

    Add save file

--- src/main/java/com/example/spring_boot_project/DocumentRepository.java
+++ src/main/java/com/example/spring_boot_project/DocumentRepository.java
@@ -1,9 +1,10 @@
 package com.example.spring_boot_project;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface DocumentRepository {
-    void add(FileEntity file);
+    void add(FileEntity file) throws IOException;
     void update(FileEntity file);
     void remove(FileEntity file);
     List<FileEntity> query(FileSpecification fileSpecification);
--- src/main/java/com/example/spring_boot_project/FileSystemDocumentRepository.java
+++ src/main/java/com/example/spring_boot_project/FileSystemDocumentRepository.java
@@ -1,12 +1,18 @@
 package com.example.spring_boot_project;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class FileSystemDocumentRepository implements DocumentRepository{
     @Override
-    public void add(FileEntity fileEntity) {
+    public void add(FileEntity fileEntity) throws IOException {
         File file = new File("C:\\Users\\user\\Desktop\\files\\" + fileEntity.getOriginalName());
+        Files.write(Paths.get(file.getAbsolutePath() + "." + file.getName()), fileEntity.getFile());
     }
 
     @Override
